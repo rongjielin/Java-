a0863b9d2e85cdf69cd5d4bd47d78376d9cbff84
rongjielin <rongjiereborn@gmail.com>
2019-10-05 16:02:55 +1000

    Create Exe1.java

---
+++ 第4章/Exe1.java
@@ -0,0 +1,15 @@
+public class Exe1 {
+	public static void main(String args[]) {
+		int x = 4;
+		if(x%2 == 0) {
+			System.out.println("x是偶数");
+		}else {
+			System.out.println("x是奇数");
+		}
+	}
+
+}
+
+
+Return Result:
+x是偶数
