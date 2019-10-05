4231f4fbdeb9909bc2128af3a8b2a31cef7b1935
rongjielin <rongjiereborn@gmail.com>
2019-10-05 13:12:13 +1000

    Update Getif.java

--- 第4章/Getif.java
+++ 第4章/Getif.java
@@ -1,12 +1,12 @@
-public class Getif {
-	public static void main(String args[]) {
-		int x=45;
+public class Getif {                                                           //创建类
+	public static void main(String args[]) {                               //主方法
+		int x=45;                                                      //声明x，y变量并赋给初值
 		int y=12;
 		
-		if(x>y) {
-			System.out.println("变量x大于变量y");
+		if(x>y) {                                                      //判断x>y
+			System.out.println("变量x大于变量y");                   //如条件成立，输出信息
 		}
-		if(x<y) {
+		if(x<y) {                                                      //判断x<y
 			System.out.println("变量x小于变量y");
 		}
 	}
