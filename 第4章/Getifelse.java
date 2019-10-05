21c4f31ccb25398d7b0b63bfcedeed37ce215fc8
rongjielin <rongjiereborn@gmail.com>
2019-10-05 13:20:48 +1000

    Create Getifelse.java

---
+++ 第4章/Getifelse.java
@@ -0,0 +1,22 @@
+public class Getifelse {
+	public static void main(String args[]) {
+		int math =95;
+		int english =56;
+		if(math>60) {
+			System.out.println("数学及格了");
+		}else {
+			System.out.println("数学不及格");
+		}
+		if(english>60) {
+			System.out.println("英语及格了");
+		}else {
+			System.out.println("英语不及格");
+		}
+	}
+}
+
+
+
+Return result:
+数学及格了
+英语不及格
