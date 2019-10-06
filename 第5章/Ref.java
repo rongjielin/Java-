55c792d1f5f99548cdf9eaa2c8298760181799da
rongjielin <rongjiereborn@gmail.com>
2019-10-06 14:05:51 +1000

    Create Ref.java

---
+++ 第5章/Ref.java
@@ -0,0 +1,11 @@
+public class Ref {
+	public static void main(String args[]) {
+		String str="hello word";
+		char mychar =str.charAt(6);
+		System.out.println("字符串str中索引位置是5的字符为: "+ mychar);
+	}
+
+}
+
+Return:
+字符串str中索引位置是5的字符为: w
