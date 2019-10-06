29c3a519d6577c4e4e9797482253c90e18aabb18
rongjielin <rongjiereborn@gmail.com>
2019-10-06 12:55:35 +1000

    Create Link.java

---
+++ 第5章/Link.java
@@ -0,0 +1,14 @@
+public class Link {
+	public static void main(String args[]) {
+		int booktime = 4;                  //声明的int型变量booktime
+		float practice =2.5f;              //声明的float型变量practice
+		                                   //将字符串与整型、浮点型变量相连，并将结果输出
+		System.out.println("我每天花费"+booktime+"小时看书："+practice+"小时上机练习");
+	}
+
+}
+
+
+
+Return Result:
+我每天花费4小时看书：2.5小时上机练习
