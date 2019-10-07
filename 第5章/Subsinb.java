0a121eb1aa8cd10d32fcc2b6c33508df46358702
rongjielin <rongjiereborn@gmail.com>
2019-10-07 12:51:14 +1000

    Create Subsinb.java

---
+++ 第5章/Subsinb.java
@@ -0,0 +1,11 @@
+public class Subsinb {
+	public static void main(String args[]) {
+		String str ="Hello Word";
+		String substr =str.substring(3);
+		System.out.println(substr);
+	}
+
+}
+
+Return:
+lo Word
