f3f409a9b9ddf192677f20b85dba277971c420b2
rongjielin <rongjiereborn@gmail.com>
2019-10-07 13:00:22 +1000

    Create Subs.java

---
+++ 第5章/Subs.java
@@ -0,0 +1,11 @@
+public class Subs {
+	public static void main(String args[]) {
+		String str="Hello Word";
+		String substr=str.substring(0,3);
+		System.out.println(substr);
+	}
+
+}
+
+Return:
+Hel
