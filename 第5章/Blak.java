fc19561b9555b42d7ef4d37b52d8e259a5e7a556
rongjielin <rongjiereborn@gmail.com>
2019-10-07 13:05:54 +1000

    Create Blak.java

---
+++ 第5章/Blak.java
@@ -0,0 +1,13 @@
+public class Blak {
+	public static void main(String args[]) {
+		String str="  Java class  ";
+		System.out.println("字符原来的长度： "+str.length());
+		System.out.println("去除空格后的长度： "+str.trim().length());
+		//去除str前部和尾部的空格，然后输出结果
+	}
+
+}
+
+Return:
+字符原来的长度： 14
+去除空格后的长度： 10
